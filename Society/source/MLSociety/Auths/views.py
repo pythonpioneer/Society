89d1b00268846dc7aebb5ff7387ae0d1d3675203
Hritik <pypi.pioneer@gmail.com>
2023-01-23 22:24:10 +0530

    creating user

--- Society/source/MLSociety/Auths/views.py
+++ Society/source/MLSociety/Auths/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from Auths.models import Member
 
 # Create your views here.
+# giving login authorization
 def signin_page(request):
     return render(request, 'loginpage.html')
 
+# creating user 
 def signup_page(request):
-    return render(request, 'signuppage.html')
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password')
+        password2 = request.POST.get('confirm_password')
+
+        # now confirm the password
+        if password1 == password2:
+            # data will be pushed into database
+            user = Member.create_user(name=name, email=email, password=password1)
+            user.save()
+
+            # now redirect to login page
+            return redirect('Auth/signin')
+
+        else:
+            # show the django message "password didn't matched"
+            messages.error(request, "Password didn't match")
+            return redirect('Auth/signup')
+
+    else:
+        return render(request, 'signuppage.html')
+
